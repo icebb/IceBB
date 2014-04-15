c2d14349dd0a9896842d274941c91a9634d11fe7
Eric Hocking <erich@xaos-ia.com>
2014-04-15 16:11:32 -0400

    Create init.php
    This file will be used to auto load the functions we need, let's stop being redundant

---
+++ inc/init.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * IceBB 1.0
+ * Copyright (c) 2014 XAOS Interactive
+ *
+ * Website: http://icebb.github.io
+ * License: http://icebb.github.io/about/license
+ *
+ */
+
+//Disallow direct access to this file for security reasons
+if (!defined('IN_ICEBB'))
+{
+  die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_ICEBB is defined.");
+}
+
+?>
