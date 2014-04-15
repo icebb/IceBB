8bcc530f3eb7a9a3951fddcdc0b6288a32ce109d
Eric Hocking <erich@xaos-ia.com>
2014-04-15 16:30:31 -0400

    Create class_skin.php
    Create the new skins class, the icebb skin engine will be powered by Smarty

---
+++ inc/class_skin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * IceBB 1.0
+ * Copyright (c) 2014 XAOS Interactive
+ *
+ * Website: http://icebb.github.io
+ * License: http://icebb.github.io/about/license
+ *
+ */
+
+class skin
+{
+
+  var $output;
+  var $loaded_templates = array();
+  var $skin_id;
+  var $rss_link = '';
+  var $rss_links = array();
+  var $header_extra;
+
+  function load_skin($skin_id='')
+  {
+    global $icebb,$db,$error_handler;
+  }
+
+  function load_template($template)
+  {
+    global $icebb,$std,$error_handler;
+  }
+
+  
+
+}
+
+?>
