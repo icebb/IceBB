712001a4a17984690b868430dc0e7b3c0e9038c1
Eric Hocking <erich@xaos-ia.com>
2014-04-15 20:46:12 -0400

    Create class_core.php

---
+++ inc/class_core.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * IceBB 1.0
+ * Copyright (c) 2014 XAOS Interactive
+ *
+ * Website: http://icebb.github.io
+ * License: http://icebb.github.io/about/license
+ *
+ */
+
+class IceBB
+{
+  /**
+   * The friendly version number of IceBB we're running.
+   *
+   * @var string
+   */
+   public $version = "1.0.1";
+
+   /**
+    * The version code of IceBB we're running.
+    *
+    * @var integer
+    */
+    public $version_code = 1001;
+
+
+}
+
+?>
