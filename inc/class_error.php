27a6b0b2fa6100950b9de82a77c17e186a9f185d
Eric Hocking <erich@xaos-ia.com>
2014-04-18 09:58:46 -0400

    define custom error codes

--- inc/class_error.php
+++ inc/class_error.php
@@ -9,6 +9,16 @@
  *
  */
 
+// Set to 1 if recieving a blank page (skin failure).
+define('MANUAL_WARNINGS', 0);
+ 
+// Define Custom IceBB error handler constants with a value not used by php's error handler.
+define('ICEBB_SQL', 20);
+define('ICEBB_SKIN', 30);
+define('ICEBB_GENERAL', 40);
+define('ICEBB_NOT_INSTALLED', 41);
+define('ICEBB_NOT_UPGRADED', 42);
+
 class error_handler
 {
 
